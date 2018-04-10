06cf3159e342ad8534e9e8039b04ca5aa2e58391
cpockrandt <noreply-github@cpockrandt.de>
2018-04-11 00:49:52 +0200

    working :) and started optimizing

--- include/sdsl/rank_support_int.hpp
+++ include/sdsl/rank_support_int.hpp
@@ -79,15 +79,8 @@ inline rank_support_int<t_b, t_v>::rank_support_int(const int_vector<t_b>* v) { 
 template <typename uintX_t>
 constexpr uintX_t bm_rec(const uintX_t w, const uint8_t length, const uint8_t max_length)
 {
-    return (length >= max_length) ? w
-        : bm_rec(w | (w << length), length << 1, max_length);
+    return (length >= max_length) ? w : bm_rec(w | (w << length), length << 1, max_length);
 }
-
-// template <uint8_t t_bits, uint8_t t_value>
-// constexpr uint64_t create_bitmask()
-// {
-//     return create_bitmask_recursive(0);
-// }
 
 template <uint8_t t_b, uint8_t t_v, typename T = void>
 struct rank_support_int_trait;
@@ -96,23 +89,16 @@ template <uint8_t t_b, uint8_t t_v>
 struct rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b == 0> > {
 protected:
 	static uint64_t masks[t_v];
-	static uint64_t even_mask;
-	static uint64_t carry_select_mask;
+	constexpr static uint64_t even_mask = bm_rec<uint64_t>(bits::lo_set[t_b], t_b * 2, 64);
+	constexpr static uint64_t carry_select_mask = bm_rec<uint64_t>(1ULL << t_b, t_b * 2, 64);
 
 public:
 	typedef typename rank_support_int<t_b, t_v>::size_type size_type;
 	typedef typename rank_support_int<t_b, t_v>::value_type value_type;
 
+	// TODO(cpockrandt): make bitmasks all constexpr and remove this method
 	static void init()
 	{
-		even_mask = bits::lo_set[t_b];
-		for (uint8_t i = t_b * 2; i < 64; i <<= 1)
-			even_mask |= even_mask << i;
-
-		// even_mask = 0;//bm_rec(bits::lo_set[t_b], t_b * 2 + 3, 64);
-
-		// std::cout << std::bitset<64>(even_mask) << std::endl <<  << std::endl << std::endl;
-
 		for (value_type v = 0; v < t_v; ++v)
 		{
 			masks[v] = v;
@@ -120,14 +106,12 @@ public:
 				masks[v] |= masks[v] << i;
 		}
 
-		carry_select_mask = masks[1] << t_b;
-
 		uint64_t tmp_carry = masks[1];
 		for (value_type v = 0; v < t_v; ++v)
 			masks[v] |= tmp_carry << t_b;
 	}
 
-    static size_type set_positions(uint64_t w, const value_type v)
+    static uint64_t set_positions(uint64_t w, const value_type v)
     {
 		uint64_t res = (masks[v] - (even_mask & w)) & carry_select_mask;
 		res |= ((masks[v] - (even_mask & (w >> t_b))) & carry_select_mask) << 1;
@@ -154,8 +138,8 @@ protected:
 	static constexpr uint8_t cyclic_shifts = t_b / util::gcd(t_b, 64 % t_b);
 	static uint8_t offsets[cyclic_shifts];
 	static uint128_t masks[t_v];
-	static uint128_t even_mask;
-	static uint128_t carry_select_mask;
+	constexpr static uint128_t even_mask = bm_rec<uint128_t>(bits::lo_set[t_b], t_b * 2, 128);
+	constexpr static uint128_t carry_select_mask = bm_rec<uint128_t>(1ULL << t_b, t_b * 2, 128);
 
 public:
 	typedef typename rank_support_int<t_b, t_v>::size_type size_type;
@@ -163,10 +147,6 @@ public:
 
 	static void init()
 	{
-		even_mask = bits::lo_set[t_b];
-		for (uint8_t i = t_b * 2; i < 128; i <<= 1)
-			even_mask |= even_mask << i;
-
 		for (value_type v = 0; v < t_v; ++v)
 		{
 			masks[v] = v;
@@ -174,19 +154,17 @@ public:
 				masks[v] |= masks[v] << i;
 		}
 
-		for (uint8_t i = 1; i < cyclic_shifts; ++i)
-			offsets[i] = t_b - ((64 - offsets[i - 1]) % t_b);
-
-		carry_select_mask = masks[1] << t_b;
-
-		uint64_t tmp_carry = masks[1];
+		uint128_t tmp_carry = masks[1];
 		for (value_type v = 0; v < t_v; ++v)
 			masks[v] |= tmp_carry << t_b;
+
+		for (uint8_t i = 1; i < cyclic_shifts; ++i)
+			offsets[i] = t_b - ((64 - offsets[i - 1]) % t_b);
 	}
 
-    static size_type set_positions(uint64_t w, const value_type v)
+    static uint64_t set_positions(uint64_t w, const value_type v)
     {
-		// TODO: 128bit-masken auf 64bit casten?
+		// TODO: cast 128bit-bitmasks down to 64bit for efficiency reasons?
 		uint64_t res = (masks[v] - (even_mask & w)) & carry_select_mask;
 		res |= ((masks[v] - (even_mask & (w >> t_b))) & carry_select_mask) << 1;
 		return res;
@@ -194,9 +172,8 @@ public:
 
     static uint128_t set_positions(uint128_t w, const value_type v, const size_type word_pos)
     {
-		uint8_t offset = 64 - (t_b - offsets[word_pos % cyclic_shifts]);
-		w >>= offset;
-		uint8_t offset_for_even_and_carry = t_b - offsets[1] + ((64 / t_b) & 0x01) * t_b;
+		w >>= 64 - (t_b - offsets[word_pos % cyclic_shifts]); // offset
+
 		uint128_t res = (masks[v] - (even_mask & w)) & carry_select_mask;
 		res |= ((masks[v] - (even_mask & (w >> t_b))) & carry_select_mask) << 1;
 		return res;
@@ -205,39 +182,39 @@ public:
 	static void set(uint128_t & w, const uint64_t hi, const uint64_t lo)
 	{
 		w = hi;
-		w <<= 64;
-		w |= lo;
+		w = (w <<= 64) | lo;
 	}
 
 	static uint32_t word_rank(const uint64_t* data, const size_type idx, const value_type v)
 	{
 		size_type bit_pos = idx * t_b;
 		size_type word_pos = bit_pos >> 6;
-		if (word_pos % cyclic_shifts == 0)
+		uint8_t word_shift = word_pos % cyclic_shifts;
+		if (word_shift == 0)
 		{
 			uint64_t w = *(data + word_pos);
 			return bits::cnt(set_positions(w, v) & bits::lo_set[(bit_pos & 0x3F) + 1]);
 		}
 		else
 		{
-			size_type in_word_pos = bit_pos & 0x3F;
-
 			uint128_t w;
 			set(w, *(data + word_pos), *(data + word_pos - 1));
 
-			uint8_t w_width = in_word_pos + (t_b - offsets[(word_pos) % cyclic_shifts]) + 1; // + 1 because carry bit will be one position further to the left
+			uint8_t in_word_pos = bit_pos & 0x3F;
+			uint8_t w_width = in_word_pos + (t_b - offsets[word_shift]) + 1; // + 1 because carry bit will be one position further to the left
 
 			uint128_t lo_set128;
 			set(lo_set128, bits::lo_set[std::max((signed) 0, w_width - 64)], bits::lo_set[std::min((uint8_t)64, w_width)]);
 			uint128_t res = set_positions(w, v, word_pos) & lo_set128;
-			return bits::cnt(res >> 64) + bits::cnt(res);
+			return bits::cnt((res >> 64) | res);
 		}
 	}
 
 	// NOTE(cpockrandt): we only count full blocks!
 	static uint32_t full_word_rank(const uint64_t* data, const size_type word_pos, const value_type v)
 	{
-		if (word_pos % cyclic_shifts == 0)
+		uint8_t word_shift = word_pos % cyclic_shifts;
+		if (word_shift == 0)
 		{
 			uint64_t w = *(data + word_pos);
 			return bits::cnt(set_positions(w, v));
@@ -247,16 +224,14 @@ public:
 			uint128_t w;
 			set(w, *(data + word_pos), *(data + word_pos - 1));
 
-			// uint8_t w_width = 64 + (t_b - offsets[word_pos % cyclic_shifts]) - (t_b - offsets[(word_pos + 1) % cyclic_shifts]);
-			// wrong for last cyclic shift: uint8_t w_width = 64 - offsets[word_pos % cyclic_shifts] + offsets[(word_pos + 1) % cyclic_shifts] + 1;
-			uint8_t w_width = 64 + (t_b - offsets[word_pos % cyclic_shifts]) + 1; // + 1 because carry bit will be one position further to the left
-			if ((word_pos + 1) % cyclic_shifts) // offsets[(word_pos + 1) % cyclic_shifts] > 0
-			 	w_width -= (t_b - offsets[(word_pos + 1) % cyclic_shifts]);
+			uint8_t w_width = 64 + (t_b - offsets[word_shift]) + 1; // + 1 because carry bit will be one position further to the left
+			if (word_shift != cyclic_shifts - 1) // (word_pos + 1) has offset
+			 	w_width -= (t_b - offsets[word_shift + 1]);
 
-			uint128_t lo_set128;
+			uint128_t lo_set128; // TODO(cpockrandt): construct global array
 			set(lo_set128, bits::lo_set[std::max((signed) 0, w_width - 64)], bits::lo_set[std::min((uint8_t)64, w_width)]);
 			uint128_t res = set_positions(w, v, word_pos) & lo_set128;
-			return bits::cnt(res >> 64) + bits::cnt(res);
+			return bits::cnt((res >> 64) | res);
 		}
 	}
 };
@@ -270,14 +245,14 @@ template <uint8_t t_b, uint8_t t_v>
 uint8_t rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b != 0> >::offsets[rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b != 0> >::cyclic_shifts];
 
 template <uint8_t t_b, uint8_t t_v>
-uint64_t rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b == 0> >::even_mask;
+constexpr uint64_t rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b == 0> >::even_mask;
 template <uint8_t t_b, uint8_t t_v>
-uint128_t rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b != 0> >::even_mask;
+constexpr uint128_t rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b != 0> >::even_mask;
 
 template <uint8_t t_b, uint8_t t_v>
-uint64_t rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b == 0> >::carry_select_mask;
+constexpr uint64_t rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b == 0> >::carry_select_mask;
 template <uint8_t t_b, uint8_t t_v>
-uint128_t rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b != 0> >::carry_select_mask;
+constexpr uint128_t rank_support_int_trait<t_b, t_v, std::enable_if_t<64 % t_b != 0> >::carry_select_mask;
 
 } // end namespace sdsl
 
